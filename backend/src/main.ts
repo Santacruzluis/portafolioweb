ce815cbcb27119e86a4987bb4b91d56fbc569278
Santacruzluis <luisantacruzkids@gmail.com>
2025-06-21 16:31:22 -0400

    [FIX] Arranque de produccion 3.0

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,8 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   app.enableCors();
 
   const port = process.env.PORT || 3000;
